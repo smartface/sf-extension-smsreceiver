c6634cb479b9942f58db6bab95af9374a23ca7cb
Furkan Arabacı <furkan.arabaci@smartface.io>
2021-09-12 19:49:11 +0300

    feature(typescript): Change filepaths, add .npmignore

--- index.js
+++
@@ -1,2 +0,0 @@
-const System = require("@smartface/native/device/system");
-module.exports = require(`./smsreceiver-${System.OS}`);
---
+++ src/index.ts
@@ -0,0 +1,7 @@
+import System from "@smartface/native/device/system";
+import type SMSReceiverAndroid from './smsreceiver-Android';
+import type SMSReceiverIOS from './smsreceiver-iOS';
+
+const SMSReceiver: typeof SMSReceiverAndroid & typeof SMSReceiverIOS = require(`./smsreceiver-${System.OS}`);
+
+export default SMSReceiver;
